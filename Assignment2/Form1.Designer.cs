06e74e2df69f4d1bbecef8f3e039f9d2564a76d7
Giurgiu Matei <giurgiu.matei@yahoo.com>
2021-05-10 17:06:49 +0300

    Add files via upload

---
+++ Assignment2/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.parentView = new System.Windows.Forms.DataGridView();
+            this.childView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnUpdateDB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.parentView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // parentView
+            // 
+            this.parentView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.parentView.Location = new System.Drawing.Point(394, 12);
+            this.parentView.Name = "parentView";
+            this.parentView.RowHeadersWidth = 51;
+            this.parentView.RowTemplate.Height = 24;
+            this.parentView.Size = new System.Drawing.Size(729, 216);
+            this.parentView.TabIndex = 0;
+            // 
+            // childView
+            // 
+            this.childView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.childView.Location = new System.Drawing.Point(394, 313);
+            this.childView.Name = "childView";
+            this.childView.RowHeadersWidth = 51;
+            this.childView.RowTemplate.Height = 24;
+            this.childView.Size = new System.Drawing.Size(729, 254);
+            this.childView.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(223, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Parent";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Child";
+            // 
+            // btnUpdateDB
+            // 
+            this.btnUpdateDB.Location = new System.Drawing.Point(1238, 226);
+            this.btnUpdateDB.Name = "btnUpdateDB";
+            this.btnUpdateDB.Size = new System.Drawing.Size(122, 78);
+            this.btnUpdateDB.TabIndex = 5;
+            this.btnUpdateDB.Text = "Update DB";
+            this.btnUpdateDB.UseVisualStyleBackColor = true;
+            this.btnUpdateDB.Click += new System.EventHandler(this.btnUpdateDB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1443, 605);
+            this.Controls.Add(this.btnUpdateDB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.childView);
+            this.Controls.Add(this.parentView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.parentView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.childView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView parentView;
+        private System.Windows.Forms.DataGridView childView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnUpdateDB;
+    }
+}
+
---
+++ Assignment2/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2
+{
+    public partial class Form1 : Form
+    {
+
+        private SqlConnection sqlConnection;
+        private DataSet dataSet;
+        private SqlDataAdapter dataAdapterParent, dataAdapterChild;
+        private BindingSource bindingSourceParent, bindingSourceChild;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadConfigurationFile();
+        }
+
+        private void loadConfigurationFile()
+        {
+            sqlConnection = new SqlConnection(getConnectionString());
+            dataSet = new DataSet();
+
+            dataAdapterParent = new SqlDataAdapter(getParentQuery(), sqlConnection);
+            SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapterParent);
+            dataAdapterParent.Fill(dataSet, getParentTable());
+
+            dataAdapterChild = new SqlDataAdapter(getChildQuery(), sqlConnection);
+            commandBuilder = new SqlCommandBuilder(dataAdapterChild);
+            dataAdapterChild.Fill(dataSet, getChildTable());
+
+
+            DataRelation childParentRelation = new DataRelation("fk_child_parent", dataSet.Tables[getParentTable()].Columns[getPrimaryKeyName()],
+                dataSet.Tables[getChildTable()].Columns[getForeignKeyName()]);
+            dataSet.Relations.Add(childParentRelation);
+
+            //create bindings
+            bindingSourceParent = new BindingSource();
+            bindingSourceParent.DataSource = dataSet;
+            bindingSourceParent.DataMember = getParentTable();
+
+            bindingSourceChild = new BindingSource();
+            bindingSourceChild.DataSource = bindingSourceParent;
+            bindingSourceChild.DataMember = "fk_child_parent";
+
+            //populate views
+            parentView.DataSource = bindingSourceParent;
+            childView.DataSource = bindingSourceChild;
+
+        }
+        private void btnUpdateDB_Click(object sender, EventArgs e)
+        {
+            //fill method updates the dataset, update method updates the DB
+            dataAdapterParent.Update(this.dataSet, getParentTable());
+            dataAdapterChild.Update(this.dataSet, getChildTable());
+
+        }
+        private string getConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["connection_string"].ConnectionString.ToString();
+        }
+
+
+        private string getParentTable()
+        {
+            return ConfigurationManager.AppSettings["parent_table"];
+        }
+
+        private string getChildTable()
+        {
+            return ConfigurationManager.AppSettings["child_table"];
+        }
+
+        private string getParentQuery()
+        {
+            return ConfigurationManager.AppSettings["parent_query"];
+        }
+        private string getChildQuery()
+        {
+            return ConfigurationManager.AppSettings["child_query"];
+        }
+        private string getPrimaryKeyName()
+        {
+            return ConfigurationManager.AppSettings["parent_table_pk"];
+        }
+
+        private string getForeignKeyName()
+        {
+
+            return ConfigurationManager.AppSettings["child_table_fk"];
+
+        }
+
+
+
+       
+    }
+}
